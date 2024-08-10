d370d61d4335397587dfc2b0c3d83017378de02c
Sakshi Maharjan <sakshimaharjan.16310@gmail.com>
2024-08-10 13:10:30 +0545

    responsive code

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
