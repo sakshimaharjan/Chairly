e4317cd85ec6b7720883e03f7830951824c623f8
Sakshi Maharjan <sakshimaharjan.16310@gmail.com>
2024-11-23 16:41:28 +0545

    firebase

--- firebase.js
+++ firebase.js
@@ -1,10 +1,20 @@
+// firebase.js
+require('dotenv').config(); // Load environment variables
+
 const admin = require('firebase-admin');
-const serviceAccount = require('./chairlyCred.json');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://chairly-b1eed-default-rtdb.firebaseio.com/'
+  credential: admin.credential.cert({
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+  }),
+  databaseURL: process.env.DATABASE_URL, // Ensure this line reads from .env
 });
 
-const db = admin.database(); // For Realtime Database
+const db = admin.database();
+
 module.exports = db;
