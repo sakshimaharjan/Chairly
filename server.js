1e8e60642ba455c5b9abadbf81d06bbf93d311bb
Sakshi Maharjan <sakshimaharjan.16310@gmail.com>
2024-11-24 20:36:12 +0545

    about us page

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contact.html'));
 });
 
+// Serve the About Us page
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
 // Serve the admin page
 app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'admin.html'));
